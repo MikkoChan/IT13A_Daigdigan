90115155fb6e4036dfc4243f9ef511fc8fa70383
MikkoChan <nmikkoangelo@gmail.com>
2025-02-19 00:52:24 +0800

    Added Java Application

---
+++ Daigdigan_IT13A_activities/src/daigdigan_it13a_activities/Daigdigan_IT13A_activities.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package daigdigan_it13a_activities;
+
+/**
+ *
+ * @author binx
+ */
+public class Daigdigan_IT13A_activities {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
