dcf5982c782ee717beb72b37a2edc49ca54dc315
MikkoChan <nmikkoangelo@gmail.com>
2025-03-17 10:18:40 +0800

    Done Arrays

---
+++ Daigdigan_IT13A_activities/src/Midterm/Multidimentional_Arrays.java
@@ -0,0 +1,24 @@
+
+package Midterm;
+
+public class Multidimentional_Arrays {
+    
+        public static void main(String[] args){
+    int numbers[][] = {
+                      {1, 2, 3, 4},
+                      {5, 6, 7, 8},
+                      {9, 10, 11, 12}
+                    };
+         for (int i = 0; i < numbers.length; i++){
+            System.out.print(" The elements in row " + ( i + 1) + ": ");
+            for (int j = 0; j < numbers[i].length; j++){
+            System.out.print( numbers[i][j] + " ");
+            }
+            System.out.println();
+         }
+        }
+                
+                
+            }
+        
+  
---
+++ Daigdigan_IT13A_activities/src/Midterm/Single_Dimentional_Arrays.java
@@ -0,0 +1,21 @@
+
+package Midterm;
+
+
+public class Single_Dimentional_Arrays {
+    
+    public static void main(String[] args){
+        
+        String  Letters[] = {"A","B","C","D","E","F","G"};
+        for (int i = 0; i < Letters.length; i++){
+        System.out.print(Letters[i]);
+        if (i < Letters.length -1) {
+            System.out.print(",");
+        }
+        }
+        }
+}
+        
+    
+        
+
